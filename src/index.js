636cfa15d82f8378ce7e50562b82d5253df82293
zim89 <121241841+zim89@users.noreply.github.com>
2023-04-27 05:20:44 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App/App';
+import 'modern-normalize/modern-normalize.css';
+import './styles/base.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
