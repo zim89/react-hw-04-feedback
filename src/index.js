17c74874fe957b1c888a7e7818f04d549fd09be4
hadgi89 <71321662+hadgi89@users.noreply.github.com>
2023-04-28 11:11:31 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
-import 'modern-normalize/modern-normalize.css';
-import './styles/base.scss';
+import App from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
